0e99d4752b27abc6bb79abfece9de5a43c1e6458
DDAMDOO <sjl9524@naver.com>
2019-01-15 16:02:30 +0900

    backup

--- app/src/main/java/themollo/app/mollo/DiffuserTime.java
+++ app/src/main/java/themollo/app/mollo/DiffuserTime.java
@@ -2,8 +2,11 @@ package themollo.app.mollo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -15,15 +18,51 @@ public class DiffuserTime extends AppUtilBasement {
 
     @BindView(R.id.tvOk)
     TextView tvOk;
+    @BindView(R.id.seekBar1)
+    SeekBar sb;
+    @BindView(R.id.sbValue)
+    TextView sbVal;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_diffuser_time);
+    private static String VALUE = "5";
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.activity_diffuser_time, container, false);
         butterBind();
-        setButtonListener();
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                VALUE = (float)progress/10+"";
+                sbVal.setText(VALUE);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+        return view;
     }
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        setContentView(R.layout.activity_diffuser_time);
+//        butterBind();
+//        setButtonListener();
+//    }
 
     @Override
     public void setButtonListener() {
