cb2279670a195c4e962b76c4042fafda61cf2357
DDAMDOO <sjl9524@naver.com>
2019-01-16 11:50:52 +0900

    show current time

--- app/src/main/java/themollo/app/mollo/TimerActivity.java
+++ app/src/main/java/themollo/app/mollo/TimerActivity.java
@@ -7,31 +7,32 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class TimerActivity extends AppCompatActivity {
+
+    long now = System.currentTimeMillis();
+    // 현재시간을 date 변수에 저장한다.
+    Date date = new Date(now);
+    // 시간을 나타냇 포맷을 정한다 ( yyyy/MM/dd 같은 형태로 변형 가능 )
+    SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    // nowDate 변수에 값을 저장한다.
+    String formatDate = sdfNow.format(date);
+
+    TextView dateNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-        Handler mHandler = new Handler();
-        mHandler.postDelayed(new Runnable() {
-            //Do Something
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                Intent i = new Intent(TimerActivity.this, TimerActivity.class); // xxx가 현재 activity,
-                //yyy가 이동할 activity
-                startActivity(i);
-                finish();
-            }
-        }, 1000); // 1000ms
+        dateNow = (TextView) findViewById(R.id.txt);
+        dateNow.setText(formatDate);    // TextView 에 현재 시간 문자열 할당
     }
-
 
 //    long IAvailableDate;
 //    long ILeaveDay;
