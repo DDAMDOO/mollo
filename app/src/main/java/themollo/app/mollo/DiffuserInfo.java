8f467579a9207df9a1e669e33a00360cadc2a726
DDAMDOO <sjl9524@naver.com>
2019-01-23 17:13:26 +0900

    error Error: Type com.google.android.gms.common.internal.zzf is referenced as an interface from .

--- app/src/main/java/themollo/app/mollo/DiffuserInfo.java
+++ app/src/main/java/themollo/app/mollo/DiffuserInfo.java
@@ -1,6 +1,7 @@
 package themollo.app.mollo;
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +9,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.result.DailyTotalResult;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class DiffuserInfo extends AppCompatActivity {
     TextView textView; //결과를 띄어줄 TextView
@@ -22,6 +36,16 @@ public class DiffuserInfo extends AppCompatActivity {
     Document doc = null;
     String Temperature;//결과를 저장할 문자열변수
     TextView intent;
+
+
+
+    private static final int REQUEST_OAUTH = 1;
+    private static final String AUTH_PENDING = "auth_state_pending";
+    private boolean authInProgress = false;
+    private GoogleApiClient mClient;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +53,70 @@ public class DiffuserInfo extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.current_temp);
         reload = (TextView) findViewById(R.id.temperature);
+
+
+
+
+        if (savedInstanceState != null) {
+            authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
+        }
+
+        mClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.HISTORY_API)
+                .addApi(Fitness.CONFIG_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
+                .useDefaultAccount()
+                .addConnectionCallbacks(
+                        new GoogleApiClient.ConnectionCallbacks() {
+
+                            @Override
+                            public void onConnected(Bundle bundle) {
+
+
+                                //Async To fetch steps
+                                new FetchStepsAsync().execute();
+
+                            }
+
+                            @Override
+                            public void onConnectionSuspended(int i) {
+                                // If your connection to the sensor gets lost at some point,
+                                // you'll be able to determine the reason and react to it here.
+                                if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                }
+                            }
+                        }
+                ).addOnConnectionFailedListener(
+                        new GoogleApiClient.OnConnectionFailedListener() {
+                            // Called whenever the API client fails to connect.
+                            @Override
+                            public void onConnectionFailed(ConnectionResult result) {
+                                if (!result.hasResolution()) {
+                                    // Show the localized error dialog
+                                    GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
+                                            DiffuserInfo.this, 0).show();
+                                    return;
+                                }
+                                // The failure has a resolution. Resolve it.
+                                // Called typically when the app is not yet authorized, and an
+                                // authorization dialog is displayed to the user.
+                                if (!authInProgress) {
+                                    try {
+                                        authInProgress = true;
+                                        result.startResolutionForResult(DiffuserInfo.this, REQUEST_OAUTH);
+                                    } catch (IntentSender.SendIntentException e) {
+                                    }
+                                }
+                            }
+                        }
+                ).build();
+        mClient.connect();
+
+
+
+
+
 
         reload.setOnClickListener(new View.OnClickListener() {//onclicklistener를 연결하여 터치시 실행됨
             @Override
@@ -55,10 +143,16 @@ public class DiffuserInfo extends AppCompatActivity {
                         Log.i("TEMP", "" + Temperature);
                         textView.setText(Temperature);
                     }
+
+
                 }.execute();
 
             }
         });
+
+
+
+
         //diffuser 시간 정하는 popup으로 이동
         intent = findViewById(R.id.tvDiffuserOn);
         intent.setOnClickListener(new View.OnClickListener(){
@@ -69,5 +163,33 @@ public class DiffuserInfo extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    private class FetchStepsAsync extends AsyncTask<Object, Object, Long> {
+        protected Long doInBackground(Object... params) {
+            long total = 0;
+            PendingResult<DailyTotalResult> result = Fitness.HistoryApi.readDailyTotal(mClient, DataType.TYPE_STEP_COUNT_DELTA);
+            DailyTotalResult totalResult = result.await(30, TimeUnit.SECONDS);
+            if (totalResult.getStatus().isSuccess()) {
+                DataSet totalSet = totalResult.getTotal();
+                if (totalSet != null) {
+                    total = totalSet.isEmpty()
+                            ? 0
+                            : totalSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                }
+            } else {
+                Log.w("api", "There was a problem getting the step count.");
+            }
+            return total;
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            super.onPostExecute(aLong);
+            final TextView textView=findViewById(R.id.daily_step);
+            textView.setText(String.valueOf(aLong) );
+            //Total steps covered for that day
+        }
     }
 }
