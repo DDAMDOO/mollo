2ecaf441f03668b35510721c7da163428c3190f1
DDAMDOO <sjl9524@naver.com>
2019-02-21 16:43:51 +0900

    timer error corrected(milli second problem)

--- app/src/main/java/themollo/app/mollo/diffuser/TimerActivity.java
+++ app/src/main/java/themollo/app/mollo/diffuser/TimerActivity.java
@@ -20,7 +20,7 @@ public class TimerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-        mText=(TextView)findViewById(R.id.txt);
+        mText = (TextView) findViewById(R.id.txt);
 
         backPressController = new BackPressController(this);
 
@@ -29,9 +29,7 @@ public class TimerActivity extends Activity {
 
         //타이머에서 설정한 시간을 가져온 intent
         int cd_val = Integer.parseInt(cdVal);
-        Log.d("발",cd_val+"");
-        if(cd_val==0)
-        {
+        if (cd_val == 0) {
             Intent intent1 = new Intent(TimerActivity.this, TimerFinActivity.class);
             startActivity(intent1);
             finish();
@@ -39,20 +37,19 @@ public class TimerActivity extends Activity {
         // 핸들러를 사용하지 않고도 일정시간마다 (혹은 후에) 코스를 수행할수 있도록
         // CountDownTimer 클래스가 제공된다.
         // '총시간'  과 '인터벌(간격)' 을 주면 매 간격마다 onTick 메소드를 수행한다.
-        timer = new CountDownTimer(cd_val*1000*60, 1000){
-            int result = cd_val*60;
+        timer = new CountDownTimer(cd_val * 1000 * 61, 1000) {
+            int result = cd_val * 60;
 
             @Override
             public void onTick(long millisUntilFinished) { // 총 시간과 주기
-                result--;
-                Log.d("타이머",result+"");
-                int hour = result/3600;
-                int min = (result/60)%60;
-                int sec = result%60;
-                String text = String.format("%02d : %02d : %02d",hour, min, sec);
+                int hour = result / 3600;
+                int min = (result / 60) % 60;
+                int sec = result % 60;
+                Log.d("타이머", result + ""+hour+" "+min+" "+sec);
+                String text = String.format("%02d : %02d : %02d", hour, min, sec);
                 mText.setText(text);
-                if(result==0)
-                {
+                result--;
+                if (hour == 0 && min == 0 && sec == 0) {
                     Toast.makeText(TimerActivity.this, "타이머가 종료되었습니다.\n디퓨저를 종료합니다.", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(TimerActivity.this, TimerFinActivity.class);
                     startActivity(intent);
@@ -67,6 +64,7 @@ public class TimerActivity extends Activity {
         };
         timer.start();  // 타이머 시작
     }
+
     @Override
     public void onBackPressed() {
         timer.cancel();
